6f9e6c5309064cffac6bae6401c07e352a59472c
ren-wen <zhangrenwen0107@gmail.com>
2024-05-06 20:04:03 +0800

    feat: break-point setting

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -13,6 +13,16 @@ import { createVuetify } from 'vuetify';
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  display: {
+    mobileBreakpoint: 'md',
+    thresholds: {
+      xs: 0,
+      sm: 340,
+      md: 768,
+      lg: 768,
+      xl: 768,
+    },
+  },
   theme: {
     themes: {
       light: {
