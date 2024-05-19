6a29042eeedc215554ae0177afce14b7d0d5678b
ren-wen <zhangrenwen0107@gmail.com>
2024-05-19 20:55:19 +0800

    feat: updated code wip

---
+++ src/components/BreadCrumb.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="breadcrumb-outer mx-3">
+    <div ref="bc" class="breadcrumb">
+      <div class="breadcrumb-inner d-flex align-center justify-space-between">
+        <div class="text-h6 mx-3">{{ routerListener.value.meta.title }}</div>
+        <div class="title-path d-flex">
+          <div class="title-path-text grey--text font-weight-bold">
+            {{ routerListener.value.path }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- 代替 WATCH 監聽/更新狀態 -->
+  <div class="d-none">{{ routerListener }}</div>
+</template>
+<script setup lang="ts">
+import { useUIStore } from '@/stores/useUIStore';
+import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
+import { ref, toRefs, computed, nextTick, onMounted, reactive } from 'vue';
+import { deepClone } from '@/utils/deepClone';
+const uiStatus = useUIStore();
+const router = useRouter();
+const routerListener = computed(() => {
+  return reactive(router.currentRoute);
+});
+</script>
+<style lang="scss" scoped>
+.breadcrumb {
+  width: 100%;
+  padding: 12px 0;
+  background-color: #ddd;
+  &.is-fixed {
+    position: fixed;
+    z-index: 9;
+  }
+  &.is-no-toolbar {
+    margin-top: -72px;
+  }
+}
+.breadcrumb-inner {
+  padding: 8px 12px;
+  background-color: #d7e4fa;
+  color: #000;
+}
+.breadcrumb-push {
+  margin-top: 50px;
+}
+.title-path-text {
+  margin: 0 5px;
+  text-shadow: 0 1px 0 rgba($color: #fff, $alpha: 0.8);
+  &:not(:first-child):before {
+    content: '|';
+    position: relative;
+    left: -3px;
+  }
+}
+</style>
---
+++ src/components/Drawer.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-navigation-drawer
+    v-model="getDrawer"
+    app
+    class="drawer"
+    style="z-index: 12"
+    @update:modelValue="setDrawerStatus"
+  >
+    <v-list density="compact" nav>
+      <v-list-item
+        v-for="(menu, i) in menus"
+        :key="i"
+        :prepend-icon="menu.meta.icon"
+        :title="menu.meta.title"
+        :value="menu.meta.title"
+        @click="router.push({ path: menu.path })"
+      ></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+<script setup lang="ts">
+import { useUIStore } from '@/stores/useUIStore';
+import { storeToRefs } from 'pinia';
+import { useRouter } from 'vue-router';
+import { ref, toRefs, computed, nextTick, onMounted, reactive } from 'vue';
+import { deepClone } from '@/utils/deepClone';
+
+const uiStatus = useUIStore();
+const router = useRouter();
+
+let menus = toRefs(reactive([]));
+
+function getStoryBookRouteList() {
+  if (router?.options?.routes) {
+    const storyBookRoute = router.options.routes.find((r) => (r.name = 'storyBook'));
+    if (storyBookRoute) {
+      menus = reactive(storyBookRoute.children);
+    }
+  }
+}
+
+function setDrawerStatus(value: boolean) {
+  uiStatus.setDrawerStatus(value);
+}
+
+onMounted(async () => {
+  getStoryBookRouteList();
+});
+
+const { getDrawer } = storeToRefs(uiStatus);
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles/main.scss';
+.drawer {
+  margin-top: 50px;
+}
+.drawer :deep(.v-list .v-list-item--active) {
+  color: lighten($color-primary, 10);
+}
+
+.drawer :deep(.v-list--dense .v-list-item .v-list-item__title) {
+  font-size: 1rem;
+}
+
+.drawer
+  :deep(.v-list-group .v-list-group__header .v-list-item__icon.v-list-group__header__append-icon) {
+  min-width: auto;
+}
+
+.drawer :deep(.v-list-group__header__append-icon.v-icon.v-icon) {
+  font-size: 0.75rem;
+}
+
+.drawer :deep(.v-list-item__icon.v-list-group__header__prepend-icon) {
+  margin-right: 12px;
+}
+.drawer :deep(.v-list-group--no-action > .v-list-group__items > .v-list-item) {
+  padding-left: 50px;
+}
+.drawer
+  :deep(.v-list-group--no-action.v-list-group--sub-group > .v-list-group__items > .v-list-item) {
+  padding-left: 72px;
+}
+
+.drawer ~ :deep(.v-navigation-drawer__scrim) {
+  z-index: 10 !important;
+}
+</style>
