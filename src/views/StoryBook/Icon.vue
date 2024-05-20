cc3b422b1671cce5e5a2d3233a5aa70f691b31e2
ren-wen <zhangrenwen0107@gmail.com>
2024-05-20 16:11:17 +0800

    feat: updated code wip

--- src/views/StoryBook/Icon.vue
+++ src/views/StoryBook/Icon.vue
@@ -1,25 +1,148 @@
 <template>
   <v-card class="main-section">
-    <h3>About page</h3>
-    <v-icon>mdi-eye</v-icon>
-    <p>
-      For a list of all available icons, visit the official
-      <a href="https://pictogrammers.com/library/mdi/">Material Design Icons page.</a>
-    </p>
+    <h1 class="mb-6">常用Icon</h1>
+    <p class="mb-6">以下常用icon参考 保服系統</p>
+    <p class="mb-6">如需其他更多種類的icon 可已造訪連結搜尋：</p>
+    <a href="https://pictogrammers.com/library/mdi/">Material Design Icons page.</a>
+    <h3>使用方式</h3>
+    <code class="storybook-demo-code mb-3">
+      &lt;v-icon&gt;{{ `{\{icon-name}\}` }}&lt;/v-icon&gt;
+    </code>
+    <div class="container">
+      <v-card v-for="(icon, index) in icons" :key="index" class="icon-card">
+        <v-card-title class="icon d-flex">
+          <v-icon>{{ icon.name }}</v-icon>
+        </v-card-title>
+        <v-card-title class="name d-flex">{{ icon.name }}</v-card-title>
+      </v-card>
+    </div>
   </v-card>
 </template>
-
-<!--/*******************************Script-Start**********************************************/-->
 <script setup lang="ts">
 import { useTestStore } from '@/stores/test';
 import { storeToRefs } from 'pinia';
 import { onMounted, reactive, ref } from 'vue';
-
-const pokemon = useTestStore();
-const { testNames } = storeToRefs(pokemon);
+const icons = reactive([
+  { name: 'mdi-eye' },
+  { name: 'mdi-eye-off' },
+  { name: 'mdi-bell' },
+  { name: 'mdi-view-grid' },
+  { name: 'mdi-help-circle-outline' },
+  { name: 'mdi-menu' },
+  { name: 'mdi-target' },
+  { name: 'mdi-plus-thick' },
+  { name: 'mdi-trash-can' },
+  { name: 'mdi-square-edit-outline' },
+  { name: 'mdi-content-duplicate' },
+  { name: 'mdi-hand-pointing-right' },
+  { name: 'mdi-account-details' },
+  { name: 'mdi-close' },
+  { name: 'mdi-file-remove' },
+  { name: 'mdi-check' },
+  { name: 'mdi-image-plus' },
+  { name: 'mdi-file-import' },
+  { name: 'mdi-autorenew' },
+  { name: 'mdi-crosshairs-gps' },
+  { name: 'mdi-crosshairs-off' },
+  { name: 'mdi-format-list-bulleted-square' },
+  { name: 'mdi-account' },
+  { name: 'mdi-alert-circle' },
+  { name: 'mdi-database-search' },
+  { name: 'mdi-magnify' },
+  { name: 'mdi-check-all' },
+  { name: 'mdi-checkbox-marked-outline' },
+  { name: 'mdi-checkbox-blank-outline' },
+  { name: 'mdi-currency-usd' },
+  { name: 'mdi-vector-arrange-above' },
+  { name: 'mdi-vector-difference-ab' },
+  { name: 'mdi-clipboard-alert-outline' },
+  { name: 'mdi-trophy-award' },
+  { name: 'mdi-file-powerpoint-box' },
+  { name: 'mdi-stack-overflow' },
+  { name: 'mdi-clipboard-text-outline' },
+  { name: 'mdi-chevron-left' },
+  { name: 'mdi-chevron-down' },
+  { name: 'mdi-chevron-up' },
+  { name: 'mdi-chevron-right' },
+  { name: 'mdi-chevron-double-down' },
+  { name: 'mdi-call-split' },
+  { name: 'mdi-dots-vertical' },
+  { name: 'mdi-typewriter' },
+  { name: 'mdi-file-compare' },
+  { name: 'mdi-file-alert-outline' },
+  { name: 'mdi-message-alert-outline' },
+  { name: 'mdi-account-group' },
+  { name: 'mdi-account-details' },
+  { name: 'mdi-cached' },
+  { name: 'mdi-pencil' },
+  { name: 'mdi-pen' },
+  { name: 'mdi-gesture-double-tap' },
+  { name: 'mdi-message' },
+  { name: 'mdi-alert' },
+  { name: 'mdi-tab' },
+  { name: 'mdi-triangle' },
+  { name: 'mdi-table' },
+  { name: 'mdi-directions' },
+  { name: 'mdi-dots-horizontal' },
+  { name: 'mdi-palette' },
+  { name: 'mdi-format-letter-case' },
+  { name: 'mdi-account' },
+  { name: 'mdi-calendar' },
+  { name: 'mdi-comment-text-outline' },
+  { name: 'mdi-reload' },
+  { name: 'mdi-message-text' },
+  { name: 'mdi-stack-overflow' },
+  { name: 'mdi-clipboard-text-outline' },
+  { name: 'mdi-vector-arrange-above' },
+  { name: 'mdi-vector-difference-ab' },
+  { name: 'mdi-clipboard-alert-outline' },
+  { name: 'mdi-flag-variant' },
+  { name: 'mdi-flag-variant-outline' },
+  { name: 'mdi-file-document-outline' },
+  { name: 'mdi-file-outline' },
+  { name: 'mdi-backup-restore' },
+  { name: 'mdi-database-check' },
+  { name: 'mdi-content-copy' },
+  { name: 'mdi-view-dashboard' },
+  { name: 'mdi-briefcase' },
+  { name: 'mdi-truck-fast' },
+  { name: 'mdi-file-document-edit' },
+  { name: 'mdi-link' },
+  { name: 'mdi-cogs' },
+  { name: 'mdi-server' },
+]);
 
 onMounted(async () => {});
 </script>
-<!--/********************************Script-End*********************************************/-->
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.container {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 1rem;
+}
+
+.icon-card {
+  width: 200px;
+  height: 180px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #000000;
+  margin: 0 6px 6px 0;
+  .icon {
+    width: 30px;
+    height: 30px;
+    padding: 0;
+  }
+  .name {
+    height: 80px;
+  }
+  .v-card-title {
+    word-break: break-all;
+    white-space: normal;
+  }
+}
+</style>
--- src/views/StoryBook/Input.vue
+++ src/views/StoryBook/Input.vue
@@ -1,162 +1,46 @@
 <template>
   <v-card class="main-section">
-    <h1>Input</h1>
-    <v-container>
-      <h2>InputTextField</h2>
-      <v-form ref="Form" v-model="valid">
-        <p>valid:{{ valid }}</p>
-        <InputLabel name="Pure" />
-        <InputTextField v-model="pokemon.text" />
-        <InputLabel name="With Label" />
-        <InputTextField v-model="pokemon.text" name="Enter your text" />
-        <InputLabel name="has-changed detector" />
-        <InputTextField
-          v-model="pokemon.text"
-          :has-changed="hasChange"
-          @input="eventTest"
-          @blur="eventTest"
-          @change="change"
-        />
-        <InputLabel name="Disabled Input" />
-        <InputTextField v-model="pokemon.text" :disabled="disabled" to-upper-case />
-        <InputLabel name="Required Input" />
-        <InputTextField v-model="text" :required="required" to-upper-case />
-        <InputLabel name="Slot Input" />
-        <InputTextField v-model="pokemon.text">
-          >
-          <template #prepend>
-            <v-tooltip location="bottom">
-              <template #activator="{ props }">
-                <v-icon v-bind="props" icon="mdi-help-circle-outline"></v-icon>
-              </template>
-
-              I'm a tooltip
-            </v-tooltip>
-          </template>
-
-          <template #append-inner>
-            <v-fade-transition leave-absolute>
-              <v-progress-circular color="info" size="24" indeterminate></v-progress-circular>
-            </v-fade-transition>
-          </template>
-          <template #append>
-            <v-menu>
-              <template #activator="{ props }">
-                <v-btn v-bind="props" class="mt-n2">
-                  <v-icon icon="mdi-menu" start></v-icon>
-                  Menu
-                </v-btn>
-              </template>
-
-              <v-card>
-                <v-card-text class="pa-6">
-                  <v-btn color="primary" size="large" variant="text">
-                    <v-icon icon="mdi-target" start></v-icon>
-
-                    Click me
-                  </v-btn>
-                </v-card-text>
-              </v-card>
-            </v-menu>
-          </template>
-        </InputTextField>
-
-        <InputLabel name="Format Input" />
-        <InputFormat :value="pokemon.text" to-currency>
-          <InputTextField v-model="pokemon.text" check-taiwan-phone-number :required="required" />
-        </InputFormat>
-        Pinia pokemon.text:
-        <p>{{ pokemon.text }}</p>
-        Input.vue data text :
-        <p>{{ text }}</p>
-        <v-row class="mb-1">
-          <v-col cols="12" sm="3">
-            <v-btn @click="toggleHasChange">hasChange {{ hasChange }}</v-btn>
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn @click="toggleDisabled">disabled {{ disabled }}</v-btn>
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn @click="toggleRequired">required {{ required }}</v-btn>
-          </v-col>
-        </v-row>
-        <v-row class="mb-1">
-          <v-col cols="12" sm="3">
-            <v-btn @click="submit">validate</v-btn>
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn @click="reset">reset</v-btn>
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn @click="clear">clearData</v-btn>
-          </v-col>
-          <v-col cols="12" sm="3">
-            <v-btn @click="format">formatData</v-btn>
-          </v-col>
-        </v-row>
-
-        <h2>InputAutoComplete</h2>
-        <p>items 一律只接受格式： [{text:'XXX',value:'XXX'}] 才不會亂</p>
-        <p>Bind value: {{ inputAutoCompleteValue }}</p>
-        <InputLabel name="has-changed,required" />
-        <InputAutoComplete
-          v-model="inputAutoCompleteValue"
-          :items="items"
-          :has-changed="hasChange"
-          required
-        ></InputAutoComplete>
-        <InputLabel name="progress-circular,no-data" />
-        <InputAutoComplete v-model="inputAutoCompleteValue" :items="[]">
-          <template #append-inner>
-            <v-fade-transition leave-absolute>
-              <v-progress-circular color="info" size="24" indeterminate></v-progress-circular>
-            </v-fade-transition>
-          </template>
-        </InputAutoComplete>
-        <InputLabel name="chips" />
-        <InputAutoComplete
-          v-model="inputAutoCompleteValue"
-          :items="items"
-          chips
-          clearable
-          multiple
-        ></InputAutoComplete>
-      </v-form>
-    </v-container>
+    <h1>輸入框/表單</h1>
+    <InputNumber v-model="numberFormat" />
+    <InputTextField v-model="numberFormat" toFixed />
+    <v-container></v-container>
   </v-card>
 </template>
 
-<!--/*******************************Script-Start**********************************************/-->
 <script setup lang="ts">
 import { useTestStore } from '@/stores/test';
 import { storeToRefs } from 'pinia';
 import { onMounted, reactive, ref } from 'vue';
-
-const pokemon = useTestStore(); // 綁定資料在 pinia store
-
-const text = ref('固定1111'); // 綁定資料在本組件;
-const disabled = ref(false); // 綁定資料在本組件;
-const required = ref(false); // 綁定資料在本組件;
-const hasChange = ref(false); // 綁定資料在本組件;
-
-const valid = ref(false); // 綁定資料在本組件;
+const test = useTestStore();
+import { useAlertsStore } from '@/stores/useAlertsStore';
+const alert = useAlertsStore();
+const text = ref('');
+const disabled = ref(false);
+const required = ref(false);
+const hasChange = ref(false);
+const valid = ref(false);
 const Form = ref();
-
+const testValue = ref('');
+const taiwanId = ref('');
 const inputAutoCompleteValue = ref('');
-const items = reactive([{ text: '選項1', value: '1' }]); // 綁定資料在本組件;
-
-function eventTest($event: Event) {
-  console.log(pokemon.text, $event);
-}
-function change(value: any) {
-  console.log(value);
+const items = reactive([{ text: '選項1', value: '1' }]);
+function inputEventTest($event: Event) {
+  console.log(test.text, $event);
+  alert.showMessage({ content: test.text, title: 'inputEventTest' });
 }
 
+function blurEventTest($event: Event) {
+  alert.showMessage({ content: test.text, title: 'blurEventTest' });
+  console.log(test.text, $event);
+}
+function changeEventTest($event: Event) {
+  alert.showMessage({ content: test.text, title: 'changed' });
+  console.log(test.text, $event);
+}
 function toggleHasChange() {
   hasChange.value = !hasChange.value;
   submit();
 }
-
 function toggleDisabled() {
   disabled.value = !disabled.value;
   submit();
@@ -166,19 +50,27 @@ function toggleRequired() {
   submit();
 }
 function submit() {
-  Form.value?.validate();
+  Form.value?.validate;
 }
 function reset() {
   Form.value?.reset();
 }
 function clear() {
-  pokemon.setText('');
+  test.setText('');
 }
 function format() {
-  pokemon.setText('10,000');
+  test.setText('10,080');
 }
-onMounted(async () => {});
+const numberFormat = ref('');
+const numberStepper = ref(0);
+const numberStepper2 = ref(0);
+const numberStepper3 = ref(0);
 </script>
-<!--/********************************Script-End*********************************************/-->
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.input-demo-page-card {
+  margin: 24px;
+  padding: 24px;
+  border: 1px solid #000000;
+}
+</style>
